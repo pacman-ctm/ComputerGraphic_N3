cbe0a97281c20ea7a6a5e324565f5344571b35d0
long10024070 <long10024070@gmail.com>
2022-01-06 21:39:44 +0700

    update demo Phep Chia

--- demoPhepChia/js/script.js
+++ demoPhepChia/js/script.js
@@ -130,9 +130,9 @@ document.getElementById('desDiv').addEventListener("click", function () {
 });
 
 document.getElementById('nextPage').addEventListener("click", function () {
-	location.href = "#";
+	location.href = "../../phepChia/baiTap/chia_b1.html";
 });
 
 document.getElementById('prevPage').addEventListener("click", function () {
-	location.href = "#";
+	location.href = "../../menu.html";
 });
